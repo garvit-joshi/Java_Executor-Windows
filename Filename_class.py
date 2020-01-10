a8d79c82ba48847eead85addb8daac2fdc5c51c4
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2020-01-10 20:28:30 +0530

    Create Filename_class.py

---
+++ Filename_class.py
@@ -0,0 +1,11 @@
+import glob, os
+os.chdir("C:/Users/Garvit Joshi/Documents/JavaProgs")
+file1=open("List.txt","a")
+i=1
+for file in glob.glob("*.class"):
+	file1.write(file)
+	file1.write("\n")
+	print(i,".",file)
+	i=i+1;
+file1.write("==============================\n")
+file1.write("******************************")
