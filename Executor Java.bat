bdd1b5bb85ee5f016f3edf519ecc50ab44f9c8ce
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2020-01-10 20:26:13 +0530

    Create Executor Java.bat

---
+++ Executor Java.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+ECHO                                        WELCOME TO EXECUTOR
+ECHO                                                         -Garvit Joshi
+cd C:\Users\Garvit Joshi\Documents\JavaProgs
+:first
+ECHO Name Of Java Executable Files Present In Folder Are:
+python Filename_java.py
+set /p "input=>Enter The File You Want To Execute:"
+javac %input%.java
+ECHO Name Of Python Executable Class Present In Folder Are:
+python Filename_class.py
+set /p "input=>Enter The Class You Want To Run:"
+ECHO ===============================
+ECHO OUTPUT:
+ECHO ===============================
+java %input%
+ECHO ===============================
+pause
+ECHO =======================================================
+ECHO *******************************************************
+ECHO =======================================================
+goto first
