a802a1d3a1a40133394e46a2135456ba25cf4ad1
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2020-02-13 18:35:23 +0530

    Update Executor Java.bat

--- Executor Java.bat
+++ Executor Java.bat
@@ -1,16 +1,23 @@
 @ECHO OFF
+color F0
 ECHO                                        WELCOME TO EXECUTOR
 ECHO                                                         -Garvit Joshi(garvitjoshi9@gmail.com)
 ECHO                                                          USER:%USERNAME%
 cd %cd% 
 :first
-ECHO Looking For Files In Directory:%cd%
+ECHO LOOKING FOR FILES IN:%cd%
+color F0
 ECHO Name Of Java Executable Files Present In Folder Are:
 python Filename_java.py
 set /p "input=Enter The File You Want To Execute:"
+ECHO ===============================
 javac %input%.java
+ECHO ===============================
 ECHO Name Of Java Executable Class Present In Folder Are:
 python Filename_class.py
+ECHO ===============================
+javac %input%.java
+ECHO ===============================
 set /p "input=Enter The Class You Want To Run:"
 color 0A
 ECHO ===============================
