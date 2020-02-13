7a3430558f46dee248ee4015afda47c11c8a9090
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2020-02-13 19:27:29 +0530

    Update Filename_java.py

--- Filename_java.py
+++ Filename_java.py
@@ -1,11 +1,18 @@
 import glob, os
 import sys
 a=sys.path
+count=0
 os.chdir(a[0])
 file1=open("List.txt","w")
 i=1
 for file in glob.glob("*.java"):
 	file1.write(file)
 	file1.write("\n")
-	print(i,".",file)
-	i=i+1;
+	if(count%2==0):
+		print(i,".",file,end="")
+		g=str(i)
+		l=len(file)+len(g)
+	else:
+		print("".rjust(40-l,' '),i,".",file)
+	i=i+1
+	count=count+1
