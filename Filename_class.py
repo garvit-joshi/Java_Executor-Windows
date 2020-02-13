878e06a71ee48012f8d649b3c54db77576e815d4
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2020-02-13 19:26:53 +0530

    Update Filename_class.py

--- Filename_class.py
+++ Filename_class.py
@@ -1,13 +1,21 @@
 import glob, os
 import sys
 a=sys.path
+count=0
+l=0
 os.chdir(a[0])
 file1=open("List.txt","a")
 i=1
 for file in glob.glob("*.class"):
 	file1.write(file)
 	file1.write("\n")
-	print(i,".",file)
-	i=i+1;
+	if(count%2==0):
+		print(i,".",file,end="")
+		g=str(i)
+		l=len(file)+len(g)
+	else:
+		print("".rjust(40-l,' '),i,".",file)
+	i=i+1
+	count=count+1
 file1.write("==============================\n")
 file1.write("******************************")
