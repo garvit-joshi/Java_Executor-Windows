aaba62f9b86c27721d2b37fcee64bcdf49158c35
Garvit Joshi <garvitjoshi9@gmail.com>
2020-05-26 20:18:59 +0530

    Started Adding Linux Support

---
+++ Linux/Filename_java.py
@@ -0,0 +1,32 @@
+#GitHub Link:https://github.com/garvit-joshi/Java_Executor
+import glob, os
+import sys
+a=sys.path
+count=0
+os.chdir(a[0])
+ifile=open("Input.txt","w")
+i=1
+for file in glob.glob("*.java"):
+	if(count%2==0):
+		print(i,".",file,end="")
+		g=str(i)
+		l=len(file)+len(g)
+	else:
+		print("".rjust(40-l,' '),i,".",file)
+	i=i+1
+	count=count+1
+print("")
+flag=1
+while flag==1:
+	f=int(input("Enter The File No. You Want To Execute:"))
+	i=1
+	for file in glob.glob("*.java"):
+		if(i==f):
+			flag=0
+			x=file
+			break
+		i=i+1
+	if flag==1:
+		print("File Not Found!!\nPlease Enter Again:")
+ifile.write(x)
+ifile.close()
---
+++ Linux/Java_Executor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+input="home/github/Java_Executor/Linux/Inout.txt"
+echo Welcome $USERNAME
+python3 Filename_java.py
+python3 Filename_class.py
+exit
