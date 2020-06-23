1a9de63d57622d6c06562cd6576fdf61808cfecc
Garvit Joshi <garvitjoshi9@gmail.com>
2020-06-23 21:01:20 +0530

    Fixed Bugs is Linux Subsystem

--- For Linux/Java_Executor.sh
+++ For Linux/Java_Executor.sh
@@ -2,16 +2,25 @@
 echo WELCOME TO EXECUTOR
 echo Garvit Joshi\(garvitjoshi9@gmail.com\)
 echo USER:$USERNAME
-python3 Filename_java.py
-filename="Input.txt"
-while IFS= read -r line
+while true
 do
-        javac "$line"
-done <"$filename"
-python3 Filename_class.py
-filename1="Input.txt"
-while IFS= read -r line
-do
-        java "$line"
-done <"$filename1"
+	python3 Filename_java.py
+	filename="Input.txt"
+	while IFS= read -r line
+	do
+	        javac "$line"
+	done <"$filename"
+	python3 Filename_class.py
+	filename1="Input.txt"
+	echo "-------------------------------"
+	echo "OUTPUT:"
+	echo "-------------------------------"
+	while IFS= read -r line
+	do
+	        java "$line"
+	done <"$filename1"
+	echo "-------------------------------"
+	echo "-------------------------------"
+	read -p "Press [Enter] key to continue..." readEnterKey
+done
 exit
