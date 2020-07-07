8fe7d4d087259992e194e124ba39d95518fc79dc
Garvit Joshi <garvitjoshi9@gmail.com>
2020-07-07 19:54:08 +0530

    Updated working link of Repo

--- Filename_java.py
+++ Filename_java.py
@@ -16,7 +16,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 '''
-#GitHub Link:https://github.com/garvit-joshi/Java_Executor
+#GitHub Link: https://github.com/garvit-joshi/Java_Executor-Windows
 import glob, os
 import sys
 a=sys.path
